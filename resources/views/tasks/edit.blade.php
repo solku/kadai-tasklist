988434fb79e6c537ba8fccdb0819dd7b031ac844
ryuuou.souda <rso308.ksg@gmail.com>
2018-12-25 14:54:48 +0000

    add column and validation

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -12,6 +12,17 @@
                     {!! Form::label('content', 'タスク:') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
                 </div>
+                
+                <div class="form-group">
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::select('status', [
+                        '新規' => '新規',
+                        '進行中' => '進行中',
+                        '終了' => '終了',
+                        '保留' => '保留',
+                        ],
+                        '新規', ['class' => 'form-control']) !!}
+                </div>
         
                 {!! Form::submit('更新', ['class' => 'btn btn-light']) !!}
         
